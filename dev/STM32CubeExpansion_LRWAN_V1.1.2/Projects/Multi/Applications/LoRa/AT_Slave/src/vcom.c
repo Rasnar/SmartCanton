c0220d272cbfd7ad731cd181516a5b4ceeb58ce4
Da Silva Andrade David <dasilvaandraded@gmail.com>
2017-10-24 13:40:44 +0200

    [LoRa] Replace last parts mentionning LPUART1 instead of USART1

--- dev/STM32CubeExpansion_LRWAN_V1.1.2/Projects/Multi/Applications/LoRa/AT_Slave/src/vcom.c
+++ dev/STM32CubeExpansion_LRWAN_V1.1.2/Projects/Multi/Applications/LoRa/AT_Slave/src/vcom.c
@@ -96,7 +96,7 @@ static void receive(char rx);
 
 void vcom_Init(void)
 {
-  LL_USART_InitTypeDef LPUART_InitStruct;
+  LL_USART_InitTypeDef USART_InitStruct;
 
   /*## Configure the UART peripheral ######################################*/
   /* Put the UART peripheral in the Asynchronous mode (UART Mode) */
@@ -110,26 +110,26 @@ void vcom_Init(void)
   /*
    * Clock initialization:
    * - LSE source selection
-   * - LPUART clock enable
+   * - USART clock enable
    * - GPIO clocks are enabled in vcom_IoInit()
    */
   LL_RCC_SetUSARTClockSource(LL_RCC_USART1_CLKSOURCE_HSI);
   UARTX_CLK_ENABLE();
   vcom_IoInit();
 
-  LPUART_InitStruct.BaudRate = 9600;
-  LPUART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
-  LPUART_InitStruct.StopBits = LL_USART_STOPBITS_1;
-  LPUART_InitStruct.Parity = LL_USART_PARITY_NONE;
-  LPUART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
-  LPUART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+  USART_InitStruct.BaudRate = 9600;
+  USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
+  USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
+  USART_InitStruct.Parity = LL_USART_PARITY_NONE;
+  USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
+  USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
 
-  if (LL_USART_Init(UARTX, &LPUART_InitStruct) != SUCCESS)
+  if (LL_USART_Init(UARTX, &USART_InitStruct) != SUCCESS)
   {
     Error_Handler();
   }
 
-  /* Configuring the LPUART specific LP feature - the wakeup from STOP */
+  /* Configuring the USART specific LP feature - the wakeup from STOP */
   LL_USART_EnableInStopMode(UARTX);
 
   /* WakeUp from stop mode on start bit detection*/
