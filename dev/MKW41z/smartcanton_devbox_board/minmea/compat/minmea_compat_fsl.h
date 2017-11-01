2b4fc70ca0d89de26c13def4811512139c93ab46
Da Silva Andrade David <dasilvaandraded@gmail.com>
2017-11-01 11:15:21 +0100

    [KW41z] Minean library with Freescale time compatibility file

---
+++ dev/MKW41z/smartcanton_devbox_board/minmea/compat/minmea_compat_fsl.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2017 Kosma Moczek <kosma@cloudyourcar.com>
+ * This program is free software. It comes without any warranty, to the extent
+ * permitted by applicable law. You can redistribute it and/or modify it under
+ * the terms of the Do What The Fuck You Want To Public License, Version 2, as
+ * published by Sam Hocevar. See the COPYING file for more details.
+ */
+
+
+#if !defined(HAVE_STRUCT_TIMESPEC)
+struct timespec {
+    time_t tv_sec;
+    long tv_nsec;
+};
+#endif
+
+#define inline __inline
+#define timegm mktime
+
+
+/* vim: set ts=4 sw=4 et: */
