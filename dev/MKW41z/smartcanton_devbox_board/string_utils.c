b957ac9de48f1a7790242d0df01041049cbf330b
Da Silva Andrade David <dasilvaandraded@gmail.com>
2017-11-13 20:33:32 +0100

    [KW41z] New functions to manipulate strings

---
+++ dev/MKW41z/smartcanton_devbox_board/string_utils.c
@@ -0,0 +1,37 @@
+#include "string_utils.h"
+#include "ctype.h"
+
+int convertHexStringToBytesArray(char *strHex, uint8_t* bytesArray)
+{
+	const char separator[2] = ":";
+	char *token;
+	int idx = 0;
+	unsigned int data;
+
+	/* get the first token */
+	token = strtok(strHex, separator);
+
+	/* walk through other tokens */
+	while( token != NULL ) {
+		sscanf(token, "%x", &data);
+
+		bytesArray[idx++] = (uint8_t)data;
+		token = strtok(NULL, separator);
+	}
+
+	return idx;
+}
+
+int convertBytesArrayToHexString(uint8_t *buffer, uint16_t bufferLength, char* str){
+
+	int i;
+
+	if (bufferLength > 1) {
+		for (i = 0; i < bufferLength - 1; i++) {
+			sprintf(&str[3*i], "%02X:", buffer[i]);
+		};
+	}
+
+	sprintf(&str[3*i], "%02X", buffer[i]);
+    return 3*i + 2; // String length
+}
---
+++ dev/MKW41z/smartcanton_devbox_board/string_utils.h
@@ -0,0 +1,18 @@
+/**
+ * @file    lorawan_controller_task.h
+ * @author  Da Silva Andrade David
+ * @version V1.0
+ * @date    25-10-2017
+ * @brief
+ */
+
+#ifndef __STRING_UTILS_H__
+#define __STRING_UTILS_H__
+
+#include "string.h"
+#include "stdio.h"
+
+int convertBytesArrayToHexString(uint8_t *buffer, uint16_t bufferLength, char* str);
+int convertHexStringToBytesArray(char *strHex, uint8_t* bytesArray);
+
+#endif /* __STRING_UTILS_H__ */
