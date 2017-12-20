29241267122ec137319b23a7c52eb6823434fa64
Da Silva Andrade David <dasilvaandraded@gmail.com>
2017-12-20 15:23:05 +0100

    [Android] HTTPS support for the server connection

--- dev/SmartCantonManager/app/src/main/java/com/master_hesso/smartcantonmanager/utils/Constants.java
+++ dev/SmartCantonManager/app/src/main/java/com/master_hesso/smartcantonmanager/utils/Constants.java
@@ -1,10 +1,7 @@
 package com.master_hesso.smartcantonmanager.utils;
 
 public class Constants {
-
-    public static final String BASE_URL = "http://192.168.42.207:5000/";
-//    public static final String BASE_URL = "http://192.168.2.111:5000/";
-//    public static final String BASE_URL = "http://www.eig.ch:7000/";
+    public static final String BASE_URL = "https://lsn.eig.ch:5000/";
     public static final String TOKEN = "token";
     public static final String USERNAME = "username";
     public static final String BLE_DEVICE_MAC = "ble_device_mac";
